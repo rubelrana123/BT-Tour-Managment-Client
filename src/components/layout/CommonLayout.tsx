4f7ffdb0e0820fcbb76e8b74ca3108fa0b16f8e5
rubelrana123 <rubelranatpi707458@gmail.com>
2025-08-25 21:38:02 +0600

    add basic folder and ui structure

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,15 +1,18 @@
 import  { type ReactNode } from 'react'
-import Navbar from './Navbar'
+ 
 import Footer from './Footer'
+import Navbar from './Navbar'
 
 interface IProps {
   children: ReactNode
 }
 export default function CommonLayout({children} : IProps) {
   return (
-    <div>
+    <div className='min-h-screen   flex flex-col' >
         <Navbar />
-        {children}
+       <div className='grow-1'>
+         {children}
+       </div>
         <Footer />
         </div>
   )
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,11 @@
  
+ 
 
 export default function About() {
   return (
+    <div className="container mx-auto px-4 py-8">
     <div>About page here Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in assumenda corporis, magnam modi expedita. Provident animi blanditiis nisi, et officia culpa atque eius, esse similique impedit architecto, cumque dignissimos. </div>
+    <div>About page here Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in assumenda corporis, magnam modi expedita. Provident animi blanditiis nisi, et officia culpa atque eius, esse similique impedit architecto, cumque dignissimos. </div>
+    </div>
   )
 }
