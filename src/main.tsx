d02e38f1c37e99060ae5f345ee0ad927d194d6a3
rubelrana123 <rubelranatpi707458@gmail.com>
2025-08-26 23:39:33 +0600

    add redux structure & form validation

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.tsx'
 import { ThemeProvider } from './provider/theme.provider.tsx'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+ <ReduxProvider store={store}>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     
    <RouterProvider router={router} />
     </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
